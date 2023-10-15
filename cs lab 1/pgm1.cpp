49cd79d074d8e60983b5451ecc169d8a86165d88
laksha06 <143469172+laksha06@users.noreply.github.com>
2023-10-15 15:58:28 +0530

    arithmetic.cpp

--- cs lab 1/pgm1.cpp
+++ cs lab 1/pgm1.cpp
@@ -3,18 +3,10 @@ int main()
 {
 	int a,b;
 	scanf("%d%d",&a,&b);
-	printf("the sum is %d",a+b);
-	int c,d;
-	scanf("%d%d",&c,&d);
-	printf("the diff is %d",c-d);
-	int e,f;
-	scanf("%d%d",&e,&f);
-	printf("the value is %d",e/f);
-	int g,h;
-	scanf("%d%d",&g,&h);
-	printf("the product is %d",g*h);
-	int i,j;
-	scanf("%d%d",&i,&j);
-	printf("the value is %d",i%j);
+	printf("the sum is %d\n",a+b);
+	printf("the difference is %d\n",a-b);
+	printf("the quotient is %d\n",a/b);
+	printf("the product is %d\n",a*b);
+	printf("the modulo is %d\n",a%b);
 	return 0;
 }
