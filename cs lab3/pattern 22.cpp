abfdf8074166860969e694ef7a1af712d101624f
laksha06 <143469172+laksha06@users.noreply.github.com>
2023-11-06 22:36:28 +0530

    Update pattern 22.cpp

--- cs lab3/pattern 22.cpp
+++ cs lab3/pattern 22.cpp
@@ -1,19 +1,31 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,n;
+	int i,j,n,a;
 	printf("enter:");
 	scanf("%d",&n);
 	for (i=1;i<=n;i++)
 		{
 			for(j=0;j<i;j++)
-				printf("%d",i);
+				{
+					if(j==i-1)
+					printf("%d",i);
+					else 
+					printf("%d*",i);
+				}
 			printf("\n");
 		}
-	for (i=0;i<n;i++)
+	a=n;
+	for (i=0;i<n;i++,a--)
 		{
-			for(;j<n-i;j--)
-				printf("%d",j);
+			for(j=0;j<n-i;j++)
+			{
+					if(j+i==(n-1))
+					printf("%d",a);
+					else 
+					printf("%d*",a);
+			}
+				
 			printf("\n");
 		}
 	return 0;
